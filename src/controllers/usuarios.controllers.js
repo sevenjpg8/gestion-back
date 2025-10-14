643b4c87373a0cdba7132812208cda678aaa04e0
Valentin <mikeallca27@gmail.com>
2025-10-14 11:11:17 -0500

    VOLVIO TODO A LA NORMALIDAD

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -1,5 +1,5 @@
 import User from '../models/Usuario/Usuario.js';
-import bcrypt from 'bcryptjs';
+import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import nodemailer from 'nodemailer';
 
